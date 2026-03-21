103b31bc40254806800b63319012aa577fe43828
Agustin Schlegel <128652306+AgustinSchlegel@users.noreply.github.com>
2026-03-21 15:27:28 -0300

    auxiliares

---
+++ wordle/src/auxiliares_BorrarAlTerminar_/ingresoDeTexto.java
@@ -0,0 +1,18 @@
+package auxiliares_BorrarAlTerminar_;
+
+public class ingresoDeTexto {
+
+    /*
+    *
+    *             String filasStr = JOptionPane.showInputDialog("Ingrese tamaño del tablero\nIngrese un numero entre 5 y 20:");
+            int tamañoTablero = Integer.parseInt(filasStr);
+            if (tamañoTablero<5||tamañoTablero>20) {
+                while (tamañoTablero<5||tamañoTablero>20) {
+                    filasStr = JOptionPane.showInputDialog("Tamaño fuera de rango\nIngrese un numero entre 5 y 20:");
+                    tamañoTablero = Integer.parseInt(filasStr);
+                }
+            }
+    *
+    * */
+
+}
