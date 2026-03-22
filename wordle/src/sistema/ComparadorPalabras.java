0d42f8cefde7495daffaa752020bdb2de937d67a
GreenTuGfa <facundofer2607@gmail.com>
2026-03-22 14:01:05 -0300

    Creacion ComparadorPalabras, EstadoLetra
    EstadoLetra:
    Contiene las tres posibles estados de una letra dentro de una palabra. CORRECTO(Verde), APARECE(Amarillo), INCORRECTO(gris/rojo)
    
    ComparadorPalabras:
    Contiene la logica principal del juego, la comparacion entre la palabra escrita por el jugador y la palabra a adivinar. La clase posee un metodo que permite obtener una array indicando en cada si la posicion de cada letra de la palabra es correcta o no

--- wordle/src/sistema/ComparadorPalabras.java
+++ wordle/src/sistema/ComparadorPalabras.java
@@ -1,24 +1,40 @@
 package sistema;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ComparadorPalabras {
 
 	String adivinar;
 	String secreto;
 	
+	Map<Character,Integer> contador;
+	
+
 	public ComparadorPalabras(String adivinar, String secreto) {
 		
 		this.adivinar = adivinar;
 		this.secreto = secreto;
+		this.contador = new HashMap<Character, Integer>();
+		
+		for (char c : secreto.toCharArray()) {
+		    contador.put(c, contador.getOrDefault(c, 0) + 1);
+		}
 	}
 	
-	public void comparar() {
+	public EstadoLetra[] comparar() {
 		
-		boolean[] acertado = new boolean[secreto.length()];
+		EstadoLetra[] palabra = new EstadoLetra[adivinar.length()];
 		
 		for (int i = 0; i < adivinar.length(); i++) {
+			
+			char a = adivinar.charAt(i);
+			
 			if(adivinar.charAt(i) == secreto.charAt(i)) {
-				acertado[i]= true;
+				palabra[i] = EstadoLetra.VERDE;
+				contador.put(a, contador.get(a) - 1);
 			}
 		}
+		return palabra;
 	}
 }
