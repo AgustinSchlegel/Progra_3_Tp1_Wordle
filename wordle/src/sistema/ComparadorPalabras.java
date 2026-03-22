e57049b5c4554a2ea36737d886b7a969b9f95c5b
Agustin Schlegel <128652306+AgustinSchlegel@users.noreply.github.com>
2026-03-22 18:18:54 -0300

    Update ComparadorPalabras.java

--- wordle/src/sistema/ComparadorPalabras.java
+++ wordle/src/sistema/ComparadorPalabras.java
@@ -23,34 +23,35 @@ public class ComparadorPalabras {
 	}
 	
 	public EstadoLetra[] comparar() {
-		
+
 		EstadoLetra[] palabra = new EstadoLetra[adivinar.length()];
-		
+
 		for (int i = 0; i < adivinar.length(); i++) {
-			
+
 			char a = adivinar.charAt(i);
-			
-			if(adivinar.charAt(i) == secreto.charAt(i)) {
+
+			if (adivinar.charAt(i) == secreto.charAt(i)) {
 				palabra[i] = EstadoLetra.VERDE;
 				contador.put(a, contador.get(a) - 1);
 			}
 		}
-		
+
 		for (int i = 0; i < adivinar.length(); i++) {
-		    char g = adivinar.charAt(i);
+			char g = adivinar.charAt(i);
 
-		    if (palabra[i] == null) { // no fue verde
+			if (palabra[i] == null) { // no fue verde
 
-		        if (contador.getOrDefault(g, 0) > 0) {
-		            palabra[i] = EstadoLetra.AMARILLO;
+				if (contador.getOrDefault(g, 0) > 0) {
+					palabra[i] = EstadoLetra.AMARILLO;
 
-		            // Resta la cantidad de veces que aparece
-		            contador.put(g, contador.get(g) - 1);
+					// Resta la cantidad de veces que aparece
+					contador.put(g, contador.get(g) - 1);
 
-		        } else {
-		            palabra[i] = EstadoLetra.ROJO;
-		        }
-		    }
+				} else {
+					palabra[i] = EstadoLetra.ROJO;
+				}
+			}
+		}
 		return palabra;
 	}
 }
