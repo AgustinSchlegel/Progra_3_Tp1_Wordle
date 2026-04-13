c1ac97f30a8d0d02b41760ebb4b7bf52ff0b7a49
Mateogonzalez09 <129783650+Mateogonzalez09@users.noreply.github.com>
2026-04-13 18:50:43 -0300

    actualizacion 13/4

--- wordle/src/main.java
+++ wordle/src/main.java
@@ -1,14 +1,13 @@
 
 import javax.swing.SwingUtilities;
 
-import interfazGrafica.Panel_Menu;
-
+import interfazGrafica.Menu;
 
 public class main {
     public static void main(String[] args) {
         //VERIFICAR SI PODEMOS REALIZARLO DE UNA MANERA MAS FACIL DE EXPLICAR
     	SwingUtilities.invokeLater(() -> {
-            new Panel_Menu().setVisible(true);
+            new Menu().setVisible(true);
         });
     }
 }
